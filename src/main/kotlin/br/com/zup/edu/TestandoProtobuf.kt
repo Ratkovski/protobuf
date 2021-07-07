9d3e1e6e0247ae0db1f14b60917e0fdb572c3361
Ratkovski <drezcry@hotmail.com>
2021-07-07 14:51:48 -0300

    testando protobuf

---
+++ src/main/kotlin/br/com/zup/edu/TestandoProtobuf.kt
@@ -0,0 +1,32 @@
+package br.com.zup.edu
+
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+fun main() {
+    val request = FuncionarioRequest.newBuilder()
+        .setNome("Yuri")
+        .setCpf("000.000.000-00")
+        .setSalario(2000.20)
+        .setAtivo(true)
+        .setCargo(Cargo.DEV)
+        .addEnderecos(
+            FuncionarioRequest.Endereco.newBuilder()
+                .setLogradouro("Rua das Tabajara")
+                .setCep("89340-000")
+                .setComplemento("Casa 20")
+                .build().toString()
+        )
+        .build()
+
+    println(request)
+    //escrevemos o objeto
+    request.writeTo(FileOutputStream("funcionario-request.bin"))
+    //lemos o objeto
+    val request2 = FuncionarioRequest.newBuilder()
+        .mergeFrom(FileInputStream("funcionario-request.bin"))
+    request2.setCargo(Cargo.GERENTE).build()
+    println(request2)
+
+
+}
